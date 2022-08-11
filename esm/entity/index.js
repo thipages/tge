c03c837c9cb247310681e78f04928a9c793d664f
thipages <thipages@gmail.com>
2022-08-11 18:48:14 +0200

    basicDrawer implementation

--- esm/entity/index.js
+++ esm/entity/index.js
@@ -10,26 +10,50 @@ const entityModel = {
     height: Number,
     //
     opacity: Number,
-    color: Color,
+    fillColor: Color,
+    strokeColor: Color,
     width: Number,
     fill: Boolean,
     //
     rotation: Number,
     scaleX: Number,
-    scaleY: Number
+    scaleY: Number,
+    getCenter
 };
 function getCenter() {
-    return [
-        (this.x + this.width)/2,
-        (this.y + this.height)/2
-    ];
+    return {
+        x: (this.x + this.width)/2,
+        y: (this.y + this.height)/2
+    };
 }
-const rectModel= {
-    ... entityModel,
-    getCenter,
+/**
+ *
+ * @param {*} ctx  CanvasRenderingContext2D
+ */
+function basicDrawer(ctx, shape) {
+    if (this.fill) {
+        ctx.beginPath();
+        ctx.fillStyle=this.color;
+        shape(ctx);
+        ctx.fill();
+    } else {
+        ctx.strokeColor=this.color;
+        shape(ctx);
+        ctx.stroke();
+    }
 }
-const circleModel= {
-    ... entityModel,
-    getCenter,
-    radius: Number,
+/**
+ *
+ * @param {*} ctx  CanvasRenderingContext2D
+ */
+function circle(ctx) {
+    const center = this.getCenter();
+    ctx.arc(center.x, center.y, this.width/2, 0, 0, 2 * Math.PI)
+}
+/**
+ *
+ * @param {*} ctx  CanvasRenderingContext2D
+ */
+ function rectangle(ctx) {
+    ctx.rect(this.x, this.y, this.width, this.height)
 }
